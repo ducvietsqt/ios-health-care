50cac1762dca13c10115bfa6b187b30d168d1637
viet <ducvietsqt@gmail.com>
2022-04-14 00:26:32 +0700

    feat: init project

---
+++ HealthCareViewer/Modules/SignIn/Controller/SingInViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SingInViewController.swift
+//  HealthCareViewer
+//
+//  Created by viet on 05/03/2022.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+        
+    }
+}
